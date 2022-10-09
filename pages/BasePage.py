11d67b5bb1843cfc1738bc2a7bc58725fe647085
qianfengzhen <qianfengzhen@cvte.com>
2022-10-09 19:05:17 +0800

    增加find的重试机制/弹窗机制

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -11,15 +11,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 from driver.Client import AndroidClient
+import re
 
 
 class BasePage(object):
+    element_black = [(By.XPATH, "ddd")]  # 黑名单标记
+
     def __init__(self):
         self.driver = AndroidClient.driver
 
     def find(self, kv) -> WebElement:
         # todo:处理各类弹窗
         return self.driver.find_element(*kv)
+
+    def finds(self, by, value):
+        element: WebElement
+        # 加上重试机制
+
+        for i in range(3):
+            try:
+                element = self.driver.find_element(by, value)
+                return element
+            except:
+                # 找到页面的最顶层的元素进行点击
+                # 动态变化位置的元素
+                # self.driver.page_source
+                # get_maxdepth_element.click()
+
+                # 黑名单
+                ##//*[@text='弹窗']/..///*[@text = '确认']
+                for e in BasePage.element_black:
+                    elements = self.driver.find_element(*e)
+                    if (elements.__sizeof__() > 0):
+                        elements[0].click()
 
     def findByText(self, text) -> WebElement:
         return self.find((By.XPATH, "//*[@text='%s']" % text))
@@ -31,7 +55,7 @@ class BasePage(object):
         po_elements = dict()
         if po_data.keys().__contains__("elements"):
             po_elements = po_data['elements']
-        #po_elements从外部文件读取  =yaml.safe_load(file)
+        # po_elements从外部文件读取  =yaml.safe_load(file)
 
         for step in po_method:
             step: dict
@@ -42,13 +66,13 @@ class BasePage(object):
             element = self.driver.find_element(by=element_platform['by'], value=element_platform['locator'])
             action = str(step['action']).lower()
 
-            #todo:定位失败，多数是弹框，try catch后进入一个弹框处理
+            # todo:定位失败，多数是弹框，try catch后进入一个弹框处理
             if action == "click":
                 element.click()
             elif action == "send_keys":
                 text = str(step['text'])
                 for k, v in kwargs.items():
-                    text = text.replace("$%s" %k, v)
+                    text = text.replace("$%s" % k, v)
                     print(text)
                 element.send_keys(text)
             else:
